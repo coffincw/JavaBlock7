ceb46a443614d93b286ce9f162eaacd9399686e9
coffincw <coffincw@gmail.com>
2016-12-21 11:00:07 -0500

    Added Less than operator

---
+++ src/RPNLessThanOperator.java
@@ -0,0 +1,25 @@
+/**
+ * Created by coffincw on 12/21/16.
+ */
+public class RPNLessThanOperator extends RPNOperator {
+    public RPNLessThanOperator() {
+        super("<", 2);
+    }
+
+    public void evaluate(RPNStack stack) {
+        RPNStackItem item1 = stack.pop();
+        RPNStackItem item2 = stack.pop();
+
+        double d1 = item1.getDoubleValue();
+        double d2 = item2.getDoubleValue();
+        double result;
+        if (d2 < d1) {
+            result = 1;
+        }
+        else {
+            result = 0;
+        }
+
+        stack.push(new RPNStackItem(result));
+    }
+}
